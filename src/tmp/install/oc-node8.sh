506b70689be8bff12fecee8fbcf6182d919482f3
Joern Ott <joern.ott@ott-consult.de>
2017-11-07 15:18:13 +0000

    Fix typo in install script

--- src/tmp/install/oc-node8.sh
+++ src/tmp/install/oc-node8.sh
@@ -6,7 +6,7 @@ chmod a+rwx /tmp
 curl -sSo /tmp/install/functions.sh https://raw.githubusercontent.com/joernott/docker-oc-install-library/master/install_functions.sh
 source /tmp/install/functions.sh
 
-add repos EPEL nodejs
+add_repos EPEL nodejs
 
 install_software ca-certificates nodejs
 cleanup
